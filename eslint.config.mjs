d5b4c1096316f68cc023da2602443c95ac47ef2d
Masaya-j9 <masaya.spectre@gmail.com>
2025-06-14 18:42:33 +0900

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(
+  // Your custom configs here
+  {
+    files: ['**/*.vue', '**/*.ts'],
+    ignores: ['**/*.log*', '.cache/**'],
+    rules: {
+      'no-console': 'warn',
+    }
+  }
+)
