f44d0364687c8a69bc2cad534d1e776788885690
Masaya-j9 <masaya.spectre@gmail.com>
2025-06-14 22:12:03 +0900

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
 export default defineNuxtConfig({
-  modules: ["@nuxt/eslint", "@nuxt/test-utils/module"],
+  modules: ['@nuxt/eslint', '@nuxt/test-utils/module'],
   devtools: { enabled: true },
-  compatibilityDate: "2025-05-15",
+  compatibilityDate: '2025-05-15',
   eslint: {
     config: {
       stylistic: {
         indent: 2,
         semi: true,
-        quotes: "single",
+        quotes: 'single',
       },
     },
   },
