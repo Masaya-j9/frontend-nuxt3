9212e59ba632fca98a43608c588d529376e5d17a
Masaya-j9 <masaya.spectre@gmail.com>
2025-06-15 16:20:41 +0900

    add sample UI

--- app.vue
+++ app.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <v-app>
+    <AppHeader />
+    <v-main>
+      <NuxtPage />
+    </v-main>
+    <AppFooter />
+  </v-app>
 </template>
+
+<script>
+import Header from '~/components/navigation/Header.vue';
+import Footer from '~/components/navigation/Footer.vue';
+
+export default {
+  name: 'IndexPage',
+  components: {
+    AppHeader: Header,
+    AppFooter: Footer,
+  },
+};
+</script>
---
+++ components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <h1>Stock App</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+};
+</script>
+
+<style scoped>
+header {
+  padding: 1rem;
+  background-color: #f5f5f5;
+}
+
+h1 {
+  margin: 0;
+  font-size: 1.5rem;
+}
+</style>
