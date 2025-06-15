d0f121b58b0189e1b17efac2f33ccb81cbb068f0
Masaya-j9 <masaya.spectre@gmail.com>
2025-06-15 16:23:06 +0900

    fix fmt

--- components/navigation/Footer.vue
+++ components/navigation/Footer.vue
@@ -1,21 +1,25 @@
 <template>
   <v-footer
-    class='text-center d-flex flex-column ga-2 pt-4 pb-2'
-    color='indigo-lighten-1'
+    class="text-center d-flex flex-column ga-2 pt-4 pb-2"
+    color="indigo-lighten-1"
   >
-    <div class='d-flex ga-3'>
+    <div class="d-flex ga-3">
       <v-btn
-        v-for='icon in icons'
-        :key='icon'
-        :icon='icon'
-        density='comfortable'
-        variant='text'
+        v-for="icon in icons"
+        :key="icon"
+        :icon="icon"
+        density="comfortable"
+        variant="text"
       />
     </div>
 
-    <v-divider class='my-2' thickness='2' width='50' />
+    <v-divider
+      class="my-2"
+      thickness="2"
+      width="50"
+    />
 
-    <div class='text-caption font-weight-regular opacity-60'>
+    <div class="text-caption font-weight-regular opacity-60">
       Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris
       cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id
       ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui.
